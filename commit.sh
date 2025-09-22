eac141aa846eb636efa6b707047cc9dcb300f055
Syphxrrd <noelle@natebeck.me>
2025-09-21 20:03:45 -0700

    "asdf"

--- commit.sh
+++ commit.sh
@@ -5,8 +5,8 @@ read -p "Commit message?: " message # Get commit message
 
 # start the commit
 
-git add .                  # adds all the files
-git commit -m ""$message"" # commits with the message
-git push origin main       # pushes to github
+git add .                    # adds all the files
+git commit -m "\"$message\"" # commits with the message
+git push origin main         # pushes to github
 
 echo "Pushed succesfully :3"
