430940165bf6259ff99d3a726b5e34e8fd3b336c
Syphxrrd <noelle@natebeck.me>
2025-09-21 19:54:12 -0700

    testing auto commiter

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAME="Auto Committer 9000"
+
+read -p "Commit message?: " message # Get commit message
+
+# start the commit
+
+git add .                # adds all the files
+git commit -m "$message" # commits with the message
+git push origin main     # pushes to github
+
+echo "Pushed succesfully :3"
